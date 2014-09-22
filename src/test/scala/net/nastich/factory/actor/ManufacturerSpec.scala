3b684c1e6b2a819bfe2b586177c483b1051e89ea
Sergey Nastich <sergey@nastich.net>
2014-09-22 04:22:52 +0400

    added test for ordering 1000 tables

--- src/test/scala/net/nastich/factory/actor/ManufacturerSpec.scala
+++ src/test/scala/net/nastich/factory/actor/ManufacturerSpec.scala
@@ -5,6 +5,8 @@ import net.nastich.factory.Settings
 import net.nastich.factory.actor.Manufacturer._
 import net.nastich.factory.common.TestKitWordSpec
 
+import scala.concurrent.duration._
+
 class ManufacturerSpec extends TestKitWordSpec {
 
   val applicationConfig = ConfigFactory.load("application")
@@ -14,6 +16,7 @@ class ManufacturerSpec extends TestKitWordSpec {
   "The Factory (manufacturer actor)" should {
     "produce chairs and it should take at least assembly time" in {
       val manufacturer = system.actorOf(Manufacturer.props(settings))
+
       manufacturer ! Shop(Chair)
       expectNoMsg(ManufacturingTime + AssemblyDuration)
       expectMsgPF() { case OrderComplete(Chair, _) => () }
@@ -21,9 +24,22 @@ class ManufacturerSpec extends TestKitWordSpec {
 
     "produce tables and it should take at least assembly time" in {
       val manufacturer = system.actorOf(Manufacturer.props(settings))
+
       manufacturer ! Shop(Table)
       expectNoMsg(ManufacturingTime + AssemblyDuration)
       expectMsgPF() { case OrderComplete(Table, _) => () }
+    }
+
+    "be able to produce many tables" in {
+      val manufacturer = system.actorOf(Manufacturer.props(settings))
+
+      for (i <- 1 to 1000) manufacturer ! Shop(Table)
+      within (10.seconds) {
+        for (i <- 1 to 1000) {
+          expectMsgPF() { case OrderComplete(Table, _) => () }
+          if (i % 100 == 0) system.log.info(s"Received $i / 10000 responses.")
+        }
+      }
     }
   }
 
